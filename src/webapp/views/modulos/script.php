e158dc8f557e4f52eb75dccb1ebd05b0d522cfc3
andresllontop <llontopdiazandres@gmail.com>
2019-04-11 11:29:07 -0500

    style 1.1

---
+++ src/webapp/views/modulos/script.php
@@ -0,0 +1,8 @@
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="<?php echo (SERVERURL); ?>src/webapp/assets/js/plugins.js"></script>
+    <script src="<?php echo (SERVERURL); ?>src/webapp/assets/js/main.js"></script>
+    <script src="<?php echo (SERVERURL); ?>src/webapp/assets/js/lib/chart-js/Chart.bundle.js"></script>
+    <script src="<?php echo (SERVERURL); ?>src/webapp/assets/js/dashboard.js"></script>
+    <script src="<?php echo (SERVERURL); ?>src/webapp/assets/js/widgets.js"></script>
+ 
+
